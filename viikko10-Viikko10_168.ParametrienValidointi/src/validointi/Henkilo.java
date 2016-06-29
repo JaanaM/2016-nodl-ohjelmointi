e4a8bbf5aa4905fbd141fd60d132b741153cfc32
Jaana <jaanamatil@gmail.com>
2016-06-29 17:39:06 +0300

    exceptions and validation

--- viikko10-Viikko10_168.ParametrienValidointi/src/validointi/Henkilo.java
+++ viikko10-Viikko10_168.ParametrienValidointi/src/validointi/Henkilo.java
@@ -5,10 +5,18 @@ public class Henkilo {
     private String nimi;
     private int ika;
 
-    public Henkilo(String nimi, int ika) {
+    public Henkilo(String nimi, int ika) throws Exception {
+        if (nimi == null || nimi.length() == 0 || nimi.length() > 40) {
+            throw new IllegalArgumentException("Syötä nimi joka on enintään 40 pitkä");
+        } else {
+            this.nimi = nimi;
+        }
+        if (ika < 0 || ika > 120) {
+            throw new IllegalArgumentException("Syötä oikea ikä");
+        } else {
+            this.ika = ika;
+        }
 
-        this.nimi = nimi;
-        this.ika = ika;
     }
 
     public String getNimi() {
--- viikko10-Viikko10_168.ParametrienValidointi/src/validointi/Laskin.java
+++ viikko10-Viikko10_168.ParametrienValidointi/src/validointi/Laskin.java
@@ -3,20 +3,27 @@ package validointi;
 public class Laskin {
 
     public int kertoma(int luvusta) {
-
-        int kertoma = 1;
-        for (int i = 1; i <= luvusta; i++) {
-            kertoma *= i;
+        if (luvusta >= 0) {
+            int kertoma = 1;
+            for (int i = 1; i <= luvusta; i++) {
+                kertoma *= i;
+            }
+            return kertoma;
+        } else {
+            throw new IllegalArgumentException("luvun täytyy olla positiivinen");
         }
 
-        return kertoma;
     }
 
     public int binomikerroin(int joukonKoko, int osaJoukonKoko) {
+        if ((joukonKoko >= 0 && osaJoukonKoko >= 0) && joukonKoko > osaJoukonKoko) {
+            int osoittaja = kertoma(joukonKoko);
+            int nimittaja = kertoma(osaJoukonKoko) * kertoma(joukonKoko - osaJoukonKoko);
 
-        int osoittaja = kertoma(joukonKoko);
-        int nimittaja = kertoma(osaJoukonKoko) * kertoma(joukonKoko - osaJoukonKoko);
+            return osoittaja / nimittaja;
+        } else {
+            throw new IllegalArgumentException("lukujen pitää olla positiivisia ja joukon koko pitää olla suurempi kuin osajoukko");
+        }
 
-        return osoittaja / nimittaja;
     }
 }
