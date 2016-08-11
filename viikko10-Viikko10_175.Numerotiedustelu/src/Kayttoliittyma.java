928af1a5e8ed6c50d1f2380a5a5d1b284ba02d6f
Jaana <jaanamatil@gmail.com>
2016-08-11 13:11:43 +0300

    combining week - lists, maps, search, order - done

--- viikko10-Viikko10_175.Numerotiedustelu/src/Kayttoliittyma.java
+++ viikko10-Viikko10_175.Numerotiedustelu/src/Kayttoliittyma.java
@@ -1,4 +1,5 @@
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
@@ -20,7 +21,7 @@ public class Kayttoliittyma {
         this.lukija = lukija;
         this.numerotiedot = numerotiedot;
     }
-
+    // start
     public void kaynnista() {
         naytaValinnat();
 
@@ -33,7 +34,7 @@ public class Kayttoliittyma {
             syotteenKasittely(syote);
         }
     }
-
+    // show choises
     public void naytaValinnat() {
         System.out.println("numerotiedustelu\n"
                 + " käytettävissä olevat komennot:\n"
@@ -45,9 +46,9 @@ public class Kayttoliittyma {
                 + " 6 poista henkilön tiedot\n"
                 + " 7 filtteröity listaus\n"
                 + " x lopeta");
-
+        System.out.println("");
     }
-
+    
     private void syotteenKasittely(String syote) {
         switch (syote) {
             case "1":
@@ -66,34 +67,39 @@ public class Kayttoliittyma {
                 haeTiedot();
                 break;
             case "6":
-                poisteTiedot();
+                poistaTiedot();
                 break;
             case "7":
                 filtteroi();
                 break;
-            default:
+            default: // wrong input
                 System.out.println("Vaara syote");
 
         }
     }
-
+    // search number by name
     private void haeNumero() {
         System.out.print("kenen: ");
         String nimi = lukija.nextLine();
         List<Tiedot> numerot = numerotiedot.haeNumero(nimi);
-        for (Tiedot numero : numerot) {
-            System.out.println("  " + numero.getNumero());
+        if (numerot != null) {
+            for (Tiedot numero : numerot) {
+                System.out.println("  " + numero.getNumero());
+            }
+        } else {
+            System.out.println(" ei löytynyt");
         }
-    }
 
+    }
+    // add number
     private void lisaaNumero() {
         System.out.print("kenelle: ");
         String nimi = lukija.nextLine();
         System.out.print("numero: ");
         String numero = lukija.nextLine();
-        numerotiedot.lisaaUusi(nimi, numero);
+        numerotiedot.lisaaUusi(nimi, numero, null, null);
     }
-
+    // search person by phone
     private void haeHenkilo() {
         System.out.print("numero: ");
         String numero = lukija.nextLine();
@@ -101,30 +107,67 @@ public class Kayttoliittyma {
         if (nimi != null) {
             System.out.println(" " + nimi.getNimi());
         } else {
-            System.out.println(" Ei löytynyt");
+            System.out.println(" ei löytynyt");
         }
     }
-
+    // add address
     private void lisaaOsoite() {
         System.out.print("kenelle: ");
         String nimi = lukija.nextLine();
-        System.out.print("katu; ");
+        System.out.print("katu: ");
         String osoite = lukija.nextLine();
         System.out.print("kaupunki: ");
         String kaupunki = lukija.nextLine();
-        numerotiedot.lisaaOsoite(nimi, nimi, kaupunki);
+        numerotiedot.lisaaUusi(nimi, null, osoite, kaupunki);
     }
-
+    // get all data by name
     private void haeTiedot() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
+        System.out.print("kenen: ");
+        String nimi = lukija.nextLine();
+        // first address
 
-    private void poisteTiedot() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
+        Tiedot osoitetiedot = numerotiedot.haeOsoite(nimi);
+        if (osoitetiedot != null) {
+            if (osoitetiedot.getKatu() != null) {
+                System.out.println(numerotiedot.tulostaOsoite(osoitetiedot));
+            } else {
+                System.out.println(" osoite ei tiedossa");
+            }
+            // then phone number/s
+            List<Tiedot> numerot = numerotiedot.haeNumero(nimi);
+            if (numerot != null) {
+                System.out.println(numerotiedot.tulostaNumerot(numerot));
+            } else {
+                System.out.println(" ei puhelinta");
+            }
+        } else {
+            System.out.println(" ei löytynyt");
+        }
 
+    }
+    // delete data
+    private void poistaTiedot() {
+        System.out.print("kenet: ");
+        String nimi = lukija.nextLine();
+        // check if name is found on the list
+        if (!numerotiedot.poistaTiedot(nimi)) {
+            // not found
+            System.out.println(" ei löytynyt");
+        }
+    }
+    // search with search term
     private void filtteroi() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        System.out.println("hakusana (jost tyhjä, listataan kaikki): ");
+        String hakusana = lukija.nextLine();
+        ArrayList<String> tulokset = numerotiedot.hakuSanalla(hakusana);
+        if (!tulokset.isEmpty()) {
+            for (String tulos : tulokset) {
+                System.out.println("");
+                System.out.println(tulos);
+            }
+        } else {
+            System.out.println(" ei löytynyt");
+        }
     }
 
 }
--- viikko10-Viikko10_175.Numerotiedustelu/src/NumeroTiedustelu.java
+++ viikko10-Viikko10_175.Numerotiedustelu/src/NumeroTiedustelu.java
@@ -1,54 +1,181 @@
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author Saria
  */
 public class NumeroTiedustelu {
-    private Map<String, List <Tiedot>> tiedotNimella;
+
+    private Map<String, List<Tiedot>> tiedotNimella;
     private Map<String, Tiedot> tiedotNumerolla;
     private Map<String, Tiedot> numeroTiedot;
+
     public NumeroTiedustelu() {
         this.tiedotNimella = new HashMap<>();
         this.tiedotNumerolla = new HashMap<>();
+        this.numeroTiedot = new HashMap<>();
     }
-    public void lisaaUusi(String nimi, String numero) {
-        Tiedot uusiHenkilo = new Tiedot(nimi, numero);
-        if (!tiedotNimella.containsKey(nimi)) {
-            this.tiedotNimella.put(nimi, new ArrayList<>());
+    // add new
+    public void lisaaUusi(String nimi, String numero, String katu, String kaupunki) {
+
+        Tiedot uusiHenkilo = new Tiedot(nimi, numero, katu, kaupunki);
+        if (uusiHenkilo.getKatu() != null) {
+            lisaaOsoite(nimi, uusiHenkilo);
+        } else if (uusiHenkilo.getNumero() != null) {
+            lisaaNimitieto(nimi, uusiHenkilo);
+            lisaaNumero(numero, uusiHenkilo);
         }
-        numeroTiedot.put(nimi, uusiHenkilo);
-        tiedotNumerolla.put(numero, uusiHenkilo);
-        tiedotNimella.get(nimi).add(uusiHenkilo);
+
     }
-    public void lisaaOsoite(String nimi, String katu, String kaupunki){
-        Tiedot uusiHenkilo = new Tiedot(nimi, katu, kaupunki);
+    // add address
+    public void lisaaOsoite(String nimi, Tiedot uusiHenkilo) {
         if (!numeroTiedot.containsKey(nimi)) {
             this.numeroTiedot.put(nimi, uusiHenkilo);
         } else {
-            numeroTiedot.get(nimi).setKatu(katu);
-            numeroTiedot.get(nimi).setKaupunki(kaupunki);
+            numeroTiedot.get(nimi).setKatu(uusiHenkilo.getKatu());
+            numeroTiedot.get(nimi).setKaupunki(uusiHenkilo.getKaupunki());
         }
     }
-    public List<Tiedot> haeNumero(String nimi){
+    // get number by name
+    public List<Tiedot> haeNumero(String nimi) {
+
+        try {
+            tiedotNimella.get(nimi);
+        } catch (Exception e) {
+            return null;
+        }
         return tiedotNimella.get(nimi);
     }
-    public Tiedot haeNimi(String numero){
+    // get name by number
+    public Tiedot haeNimi(String numero) {
+        try {
+            tiedotNumerolla.get(numero);
+        } catch (Exception e) {
+            return null;
+        }
         return tiedotNumerolla.get(numero);
     }
-    public Tiedot haeOsoite(String nimi){
+    // get address by name
+    public Tiedot haeOsoite(String nimi) {
+        try {
+            numeroTiedot.get(nimi);
+        } catch (Exception e) {
+            return null;
+        }
         return numeroTiedot.get(nimi);
     }
-    
-    
+    // delete data by name
+    public boolean poistaTiedot(String nimi) {
+
+        if (numeroTiedot.containsKey(nimi)) {
+            String numero = numeroTiedot.get(nimi).getNumero();
+            numeroTiedot.remove(nimi);
+            tiedotNimella.remove(nimi);
+            tiedotNumerolla.remove(numero);
+            return true;
+        } else {
+            return false;
+        }
+    }
+    // search by term
+    public ArrayList<String> hakuSanalla(String hakusana) {
+        HashMap<String, String> hakutiedot = new HashMap<>();
+        ArrayList<String> tulokset = new ArrayList<>();
+        hakutiedot = hakuTiedoista(hakusana);
+        if (!hakutiedot.isEmpty()) {
+            ArrayList<String> nimiJarjestys = new ArrayList(hakutiedot.keySet());
+            Collections.sort(nimiJarjestys);
+            // järjestä ja lisaa numerot
+
+            for (String key : nimiJarjestys) {
+                String tiedot = "";
+                tiedot = hakutiedot.get(key) + "\n";
+
+                List<Tiedot> numerot = haeNumero(key);
+                if (numerot == null) {
+                    tiedot += "  ei puhelinta \n";
+                } else {
+                    tiedot += tulostaNumerot(numerot);
+                }
+                tulokset.add(tiedot);
+            }
+        }
+
+        return tulokset;
+
+    }
+    // add new information
+    private void lisaaNimitieto(String nimi, Tiedot uusiHenkilo) {
+        // if not containing the key
+        // make new list 
+        if (!tiedotNimella.containsKey(nimi)) {
+            this.tiedotNimella.put(nimi, new ArrayList<>());
+            if (!numeroTiedot.containsKey(nimi)) {
+                numeroTiedot.put(nimi, uusiHenkilo);
+            }
+        }
+        // add to the list
+        this.tiedotNimella.get(nimi).add(uusiHenkilo);
+    }
+    // set number to list
+    private void lisaaNumero(String numero, Tiedot uusiHenkilo) {
+        this.tiedotNumerolla.put(numero, uusiHenkilo);
+    }
+    // search from data
+    private HashMap<String, String> hakuTiedoista(String hakusana) {
+        HashMap<String, String> hakutiedot = new HashMap<>();
+        String tiedot = "";
+        for (Tiedot osoite : numeroTiedot.values()) {
+            if (osoite.getKatu() != null) { // if contains address do search
+                if (!hakusana.isEmpty()) {
+                    if (osoite.getKatu().contains(hakusana) || osoite.getKaupunki().contains(hakusana) || osoite.getNimi().contains(hakusana)) {
+                        tiedot = " " + osoite.getNimi() + "\n" + tulostaOsoite(osoite);
+                        hakutiedot.put(osoite.getNimi(), tiedot);
+                    }
+                } else {
+                    tiedot = " " + osoite.getNimi() + "\n" + tulostaOsoite(osoite);
+                    hakutiedot.put(osoite.getNimi(), tiedot);
+                }
+
+            } else {
+                if (!hakusana.isEmpty()) {
+                    if (osoite.getNimi().contains(hakusana)) {
+                        tiedot = osoite.getNimi() + "\n" + "  osoite ei tiedossa";
+                        hakutiedot.put(osoite.getNimi(), tiedot);
+                    }
+                } else {
+                    tiedot = osoite.getNimi() + "\n" + "  osoite ei tiedossa";
+                    hakutiedot.put(osoite.getNimi(), tiedot);
+                }
+
+            }
+
+        }
+        return hakutiedot;
+    }
+    // print address
+    public String tulostaOsoite(Tiedot osoite) {
+        return "  osoite: " + osoite.getKatu() + " " + osoite.getKaupunki();
+    }
+    // print numbers
+    public String tulostaNumerot(List<Tiedot> numerot) {
+        String tuloste = "  puhelinnumerot: \n";
+        for (Tiedot numero : numerot) {
+            if (!numero.getNumero().isEmpty()) {
+                tuloste += "   " + numero.getNumero() + "\n";
+            }
+        }
+        return tuloste;
+    }
 }
